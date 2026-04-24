6ca1b7968b7eb61af1fcea04aa46a513554f1dd9
Thierry Le Bris <thierry.lebris@gmail.com>
2026-04-24 12:16:19 +0200

    Initial Commit

---
+++ MDma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MDma
+//
+//  Created by Thierry on 24/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MDma/MDmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDmaApp.swift
+//  MDma
+//
+//  Created by Thierry on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MDmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
